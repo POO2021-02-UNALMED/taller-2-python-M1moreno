9b06376b157ccbd380874944598a7f0e65d7846b
Michael Moreno Valoyes <80339158+M1moreno@users.noreply.github.com>
2021-10-25 15:42:57 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,55 @@
+class Asiento:
+	color_valido = ["rojo","verde","amarillo","negro","blanco"]
+	def __init__(self, color, precio, registro):
+		self.color = color
+		self.precio = precio
+		self.registro = registro
+
+	def cambiarColor(self, color):
+		if color in color_valido:
+			self.color = color
+
+class Auto:
+	cantidadCreados = 0
+	def __init__(self, modelo, precio, asientos, marca, motor, registro):
+		self.modelo = modelo
+		self.precio = precio
+		self.asientos = asientos
+		self.marca = marca
+		self.motor = motor
+		self.registro = registro
+
+	def cantidadAsientos(self):
+		cantidad = 0
+		for asiento in asientos:
+			if (type(asiento) is Asiento):
+				cantidad += 1
+		return cantidad
+
+	def verificarIntegridad(self):
+		integridad = True
+		if (self.registro != self.motor.registro):
+			integridad = False
+		for asiento in asientos:
+			if (type(asiento) is Asiento):
+				if (self.registro != asiento.registro):
+					integridad = False
+
+		if integridad:
+			return "Auto original"
+		else:
+			return "Las piezas no son originales"
+
+class Motor:
+	def __init__(self, numeroCilindros, tipo, registro):
+		self.numeroCilindros = numeroCilindros
+		self.tipo = tipo
+		self.registro = registro
+
+	def cambiarRegistro(self, registro):
+		self.registro = registro
+
+	def asignarTipo(self, tipo):
+		tipoValido = ["electrico", "gasolina"]
+		if (tipo in tipoValido):
+			self.tipo = tipo
